6495fedc492a1752a83354e874f45085ffaf7edb
Artem Avakyan <artem@aprint.pro>
2020-07-16 17:44:22 +0300

    create create data and init calc component

--- src/app.vue
+++ src/app.vue
@@ -1,16 +1,32 @@
 <script>
-import api from './api';
+import Api from "./api";
+const api = new Api();
 
 export default {
-    name: 'app',
-    created() {
-        api.get('directories/vehicle-brands').then((result) => console.log(result));
-    }
-}
+  name: "app",
+  data: () => ({
+    vehicle: {},
+  }),
+  async created() {
+    await Promise.all([
+      api.getData("directories/vehicle-categories"),
+      api.getData("directories/vehicle-brands"),
+      api.getData("directories/vehicle-models"),
+    ])
+      .then(([vehicleCategories, vehicleBrands, vehicleModels]) => {
+        this.vehicle = Object.assign(
+          vehicleCategories,
+          vehicleBrands,
+          vehicleModels
+        );
+      })
+      .catch((e) => console.log(e.message));
+  },
+};
 </script>
 
 <template>
-    <div>test</div>
+  <div>test</div>
 </template>
 
 <style scoped></style>
---
+++ src/components/Calc.vue
@@ -0,0 +1,13 @@
+<template>
+  
+</template>
+
+<script>
+    export default {
+        name: "Calc"
+    }
+</script>
+
+<style scoped>
+
+</style>
