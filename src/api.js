3523434b1e583e83712cd5dec37fd2628462a99d
Artem Avakyan <artem@aprint.pro>
2020-07-16 17:12:51 +0300

    create base Api

--- src/api.js
+++ src/api.js
@@ -1,9 +1,29 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'api/',
-    timeout: 3000,
-    headers: {'Authorization': 'Basic <TOKEN>'}
+  baseURL: "api/",
+  timeout: 3000,
+  headers: {
+    Authorization: "Basic dGVzdHRlc3RAbWF4cG9zdGVyLnJ1OlZ5cm42SmkzOFFhemdZTWU",
+  },
 });
 
-export default api;
+export default class Api {
+  getData(url) {
+    return api
+      .get(url)
+      .then((result) => {
+        return result.data.data;
+      })
+      .catch((e) => console.warn(e));
+  }
+
+  getDataWithParams(url, payload) {
+    return api
+      .post(url, payload)
+      .then((result) => {
+        return result.data.data;
+      })
+      .catch((e) => console.warn(e));
+  }
+}
